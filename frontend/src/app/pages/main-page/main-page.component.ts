7b96a8a443af1d597f3fd7336be15d76c609b65e
Valenteen Kurakevich <Valentin.Kurakevich@solbeg.com>
2022-07-21 10:12:22 +0300

    create-main-page

--- frontend/src/app/pages/main-page/main-page.component.ts
+++ frontend/src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ButtonLabel } from 'src/app/enums/button-label-enum';
+import { ProductRequirements } from 'src/app/interfaces/product-requirements';
+import { CITIES } from './cities';
+
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +13,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  public cities: string[] = CITIES;
+  public currentDate: Date = new Date;
+  public headerValue: string = 'Для поиска отеля заполните поля'
+  public searchButtonTitle: string = ButtonLabel.SEARCH;
+  public form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) {
+
+  }
 
   ngOnInit(): void {
+    this.buildForm();
+  }
+
+  public buildForm(): void {
+    this.form = this.formBuilder.group({
+      city: [this.cities[0], [Validators.required]],
+      checkInDate: [this.currentDate, [Validators.required]],
+      checkOutDate: ['', [Validators.required]],
+      amountGuests: ['', [Validators.required]],
+    });
+  }
+
+  public get checkOutDate() {
+    return this.form.get('checkOutDate');
+  }
+
+  public get amountGuests() {
+    return this.form.get('amountGuests');
+  }
+
+  public get checkInDate() {
+    return this.form.get('checkInDate');
+  }
+
+  public onSubmit(): void {
+    this.router.navigate(['hotel-list']);
   }
 
 }
